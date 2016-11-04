da221b08c07ace1cc9bf31897c8e072543ea27be
fd3s7 <B5021@oic.jp>
2016-11-04 16:17:53 +0900

    migration更新

--- database/migrations/2016_11_04_043156_members_master.php
+++ database/migrations/2016_11_04_043156_members_master.php
@@ -14,19 +14,19 @@ class MembersMaster extends Migration
     public function up()
     {
       Schema::create('members_master', function (Blueprint $table) {
-        $table->integer('member_id')->primary();
-        $table->string('member_mail')->unique();
-        $table->string('member_name');
-        $table->string('member_kana');
-        $table->string('member_postel');
-        $table->string('member_address1');
-        $table->string('member_address2');
-        $table->string('member_address3')->nullable();
-        $table->integer('member_tel');
-        $table->integer('gender_id')->references('id')->on('gender_master');
-        $table->integer('member_birth');
-        $table->timestamps();
-        $table->string('member_password');
+        $table->integer('member_id')->primary(); //会員ID
+        $table->string('member_mail')->unique(); //メールアドレス
+        $table->string('member_name'); //氏名
+        $table->string('member_kana'); //カナ
+        $table->string('member_postel'); //郵便番号
+        $table->string('member_address1'); //住所1
+        $table->string('member_address2'); //住所2
+        $table->string('member_address3')->nullable(); //住所3
+        $table->integer('member_tel'); //電話番号
+        $table->integer('gender_id')->references('id')->on('gender_master'); //性別ID
+        $table->integer('member_birth'); //生年月日
+        $table->timestamps(); //登録・更新
+        $table->string('member_password'); //パスワード
       });
     }
 
@@ -37,6 +37,6 @@ class MembersMaster extends Migration
      */
     public function down()
     {
-        //
+      Schema::dropIfExists('members_master');
     }
 }
---
+++ database/migrations/2016_11_04_064307_employees_master.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class EmployeesMaster extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+      Schema::create('employees_master', function (Blueprint $table) {
+        $table->integer('emoloyee_id')->primary(); //従業員ID
+        $table->string('emoloyee_name'); //従業員氏名
+        $table->string('emoloyee_kana'); //従業員カナ
+        $table->string('emoloyee_birth'); //生年月日
+        $table->integer('gender_id')->references('id')->on('gender_master'); //性別ID
+        $table->date('emoloyee_agreement_date'); //契約開始日
+        $table->date('emoloyee_agreement_enddate')->nullable(); //契約終了日
+        $table->timestamps(); //登録・更新日
+        $table->string('emoloyee_address1'); //住所1
+        $table->string('emoloyee_address2'); //住所2
+        $table->string('emoloyee_address3')->nullable(); //住所3
+      });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+      Schema::dropIfExists('employees_master');
+    }
+}
