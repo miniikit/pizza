658d729e1a278f7b79ae1f41a10d2980bfc42c31
in-the-box <hacoco345@gmail.com>
2016-11-08 00:46:41 +0900

    seed: ジャンル作成

--- database/migrations/2016_11_07_041114_genres_master.php
+++ database/migrations/2016_11_07_041114_genres_master.php
@@ -14,8 +14,9 @@ class GenresMaster extends Migration
     public function up()
     {
       Schema::create('genres_master', function (Blueprint $table) {
-        $table->integer('genre_id')->primary(); //ジャンルID
+        $table->increments('genre_id'); //ジャンルID
         $table->string('genre_name'); //ジャンル名
+        $table->timestamps();
       });
     }
 
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Illuminate\Database\Eloquent\Model;
+use Faker\Factory as Faker;
+use Carbon\Carbon;
+
+use App\Genre;
 
 class DatabaseSeeder extends Seeder
 {
@@ -11,6 +16,30 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // $this->call(UsersTableSeeder::class);
+        Model::unguard();
+
+        $this->call('GenresMasterTableSeeder');
+
+        Model::reguard();
+    }
+}
+
+// ジャンル種別
+class GenresMasterTableSeeder extends Seeder
+{
+
+    public function run()
+    {
+        DB::table('genres_master')->delete();
+
+        Genre::create([
+            'genre_name' => 'ピザ'
+        ]);
+        Genre::create([
+            'genre_name' => 'サイド'
+        ]);
+        Genre::create([
+            'genre_name' => 'ドリンク'
+        ]);
     }
 }
