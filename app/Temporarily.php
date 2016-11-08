bc8980f3ddedc192ea3bec27b643d9c3acd82316
in-the-box <hacoco345@gmail.com>
2016-11-08 14:05:33 +0900

    add: model

---
+++ app/Temporarily.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class State extends Model
+{
+
+    protected $table = 'temporaries_members_master';
+    protected $fillable = ['temporary_member_name','temporary_member_kana','temporary_member_postel','temporary_member_address1','temporary_member_address2','temporary_member_address3','temporary_member_tel'];
+}
