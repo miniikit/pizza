4ae1c545cb5395b8a466f7b75f4df5e240019773
masakisaaan <masakilex@gmail.com>
2016-11-08 16:12:26 +0900

    管理画面のテンプレートファイル作成

---
+++ resources/views/template/admin.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+
+<title>@yield('title') | 管理画面 </title>
+
+<meta name="description" itemprop="description" content="@yield('description')">
+<meta name="keywords" itemprop="keywords" content="@yield('keywords')">
+<link href="/css/common/font-awesome.min.css" rel="stylesheet">
+<link href="/css/common/bootstrap.min.css" rel="stylesheet">
+<link href="/css/common/reset.css" rel="stylesheet">
+ 
+@yield('css') 
+ 
+<script src="//ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+<script src="/js/common/bootstrap.min.js"></script>
+</head>
+
+@yield('main')
+
+</body>
+</html>
