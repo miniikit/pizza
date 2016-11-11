067d1208af8a397cba87b8f11de4ba43e1106c11
masakisaaan <masakilex@gmail.com>
2016-11-11 15:42:38 +0900

    コントローラとルート追加

--- routes/web.php
+++ routes/web.php
@@ -15,14 +15,6 @@ Route::get('/', 'IndexController@index');
 
 Route::get('/menu','MenusController@index');
 
-
-
-Route::get('/menuadd',function(){
-    return view('/menuadd/index');
-});
-
-
-
 Route::get('/company', 'PagesController@company');
 Route::get('/privacypolicy', 'PagesController@privacypolicy');
 Route::get('/agreement', 'PagesController@agreement');
@@ -31,9 +23,14 @@ Route::get('/faq', 'PagesController@faq');
 Route::get('/contact','ContactController@index');
 Route::post('/contact','ContactController@post');
 
-Route::get('/employeee/list', 'EmployeesController@employeeList'); //従業員一覧
-Route::get('/employeee/edit', 'EmployeesController@employeeEdit'); //従業員編集
-Route::get('/employeee/add', 'EmployeesController@employeeAdd'); //従業員追加
+Route::get('/employee/list', 'EmployeesController@employeeList'); //従業員一覧
+Route::get('/employee/edit', 'EmployeesController@employeeEdit'); //従業員編集
+Route::get('/employee/add', 'EmployeesController@employeeAdd'); //従業員追加
+
+Route::get('/menu/list', 'AdminMenusController@AdminMenuList'); //従業員用メニュー一覧
+Route::get('/menu/edit', 'AdminMenusController@AdminMenuEdit'); //従業員用メニュー編集
+Route::get('/menu/add', 'AdminMenusController@AdminMenuAdd'); //従業員用メニュー追加
+
 
 
 Route::get('/test','TestsController@index');
