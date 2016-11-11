9eba604636817ee7c852f310c1e2823cf129f280
masakisaaan <masakilex@gmail.com>
2016-11-11 11:56:55 +0900

    メニュー追加画面スタイリング

--- resources/views/menuadd/index.blade.php
+++ resources/views/menuadd/index.blade.php
@@ -8,7 +8,7 @@
 
 @section('main')
     <h1>メニュー追加画面</h1>
-    <div class="form-group" id="menuadd">
+    <div class="form-group table-responsive" id="menuadd">
     <table class="table">
 <!--  <thead>
     <tr>
@@ -18,12 +18,16 @@
   </thead>
   -->
   <tbody>
-    <tr>
-      <th><label for="">価格</label></th>
-      <td><input class="form-control" type="number" name="name" value=""></td>
+   <tr>
+    <th class="text-center" style="padding-top:12px;"><label for="">商品名</label></th>
+    <th><input class="form-control" type="text" name="name" value="" style="width:50%;"></th>
+    </tr>
+     <tr>
+      <th class="text-center" style="padding-top:12px;"><label for="">価格</label></th>
+      <td><input class="form-control" type="number" name="name" value="" style="width:25%;"></td>
     </tr>
     <tr>
-      <th><label for="">ジャンル</label></th>
+      <th class="text-center" style="padding-top:12px;"><label for="">ジャンル</label></th>
       <td><select name="example2">
           <option value="---">-----</option>
           <option value="選択肢1">メイン</option>
@@ -32,24 +36,20 @@
         </select></td>
     </tr>
     <tr>
-    <th><label for="">商品名</label></th>
-    <th><input class="form-control" type="text" name="name" value="" style="width:50%;"></th>
+    <th class="text-center" style="padding-top:12px;"><label for="example-date-input">販売開始日</label></th>
+    <th><input class="form-control" id="example-date-input" type="date" name="name" value="" size="5" style="width:30%"></th>
     </tr>
     <tr>
-    <th><label for="example-date-input">販売開始日</label></th>
-    <th><input class="form-control" id="example-date-input" type="date" name="name" value="" size="5"></th>
+    <th class="text-center" style="padding-top:12px;"><label for="example-date-input">販売終了日</label></th>
+    <th><input class="form-control" id="example-date-input" type="date" name="name" value="" size="5" style="width:30%"></th>
     </tr>
     <tr>
-    <th><label for="example-date-input">販売終了日</label></th>
-    <th><input class="form-control" id="example-date-input" type="date" name="name" value="" size="5"></th>
-    </tr>
-    <tr>
-    <th><label for="">商品画像</label></th>
+    <th class="text-center" style="padding-top:12px;"><label for="">商品画像</label></th>
     <th><input class="fotm-control" type="file" name"name" value""></th>
     </tr>
     <tr>
-    <th><label for="">商品説明文</label></th>
-    <th><textarea class="form-control" id="exampleTextarea" rows="6" maxlength="255" ></textarea></th>
+    <th class="text-center" style="padding-top:12px;"><label for="">商品説明文</label></th>
+    <th><textarea class="form-control" id="exampleTextarea" rows="6" maxlength="255" style="width:65%;" ></textarea></th>
     </tr>
   </tbody>
 </table>
