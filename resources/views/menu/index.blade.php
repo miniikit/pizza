036c1eaf0589c61801da153af63aaaa708f7f544
in-the-box <hacoco345@gmail.com>
2016-11-14 02:49:29 +0900

    menu:: 種別分け

--- resources/views/menu/index.blade.php
+++ resources/views/menu/index.blade.php
@@ -14,30 +14,86 @@
 @section('main')
     <div class="container menu wrap">
         <div class="productsBox">
-            <h2>PIZZA</h2>
+        <h2>PIZZA</h2>
         @foreach ($products as $product)
-            <div class="product">
-                <div class="inner">
-                    <div class="image"><img src="{{ $product->product_image }}" alt="" /></div>
-                    <div class="title"><h3>{{ $product->product_name }}</h3></div>
-                     <div class="praice"><p>{{ $product->productPrice->product_price }}円</p></div>
-                    <div class="text"><p>{{ $product->product_text }}</p></div>
-                    <div class="btn">
-                        <form class="" action="/cart/store" method="post">
-                            <span>
-                                <select class="" name="sum">
-                                    @for ($i=1; $i <= 10 ; $i++)
-                                    <option value="{{$i}}">{{$i}}</option>
-                                    @endfor
-                                </select>
-                            </span>
-                            <input type="hidden" name="id" value="{{ $product->id }}">
-                            <div class="form-bottom">カートに入れる</div>
-                            {{ csrf_field() }}
-                        </form>
+            @if ($product->genre_id == 1)
+                <div class="product">
+                    <div class="inner">
+                        <div class="image"><img src="{{ $product->product_image }}" alt="" /></div>
+                        <div class="title"><h3>{{ $product->product_name }}</h3></div>
+                         <div class="praice"><p>{{ $product->productPrice->product_price }}円</p></div>
+                        <div class="text"><p>{{ $product->product_text }}</p></div>
+                        <div class="btn">
+                            <form class="" action="/cart/store" method="post">
+                                <span>
+                                    <select class="" name="sum">
+                                        @for ($i=1; $i <= 10 ; $i++)
+                                        <option value="{{$i}}">{{$i}}</option>
+                                        @endfor
+                                    </select>
+                                </span>
+                                <input type="hidden" name="id" value="{{ $product->id }}">
+                                <div class="form-bottom">カートに入れる</div>
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
                     </div>
                 </div>
-            </div>
+            @endif
+        @endforeach
+        <h2>SIDE</h2>
+        @foreach ($products as $product)
+            @if ($product->genre_id == 2)
+                <div class="product">
+                    <div class="inner">
+                        <div class="image"><img src="{{ $product->product_image }}" alt="" /></div>
+                        <div class="title"><h3>{{ $product->product_name }}</h3></div>
+                         <div class="praice"><p>{{ $product->productPrice->product_price }}円</p></div>
+                        <div class="text"><p>{{ $product->product_text }}</p></div>
+                        <div class="btn">
+                            <form class="" action="/cart/store" method="post">
+                                <span>
+                                    <select class="" name="sum">
+                                        @for ($i=1; $i <= 10 ; $i++)
+                                        <option value="{{$i}}">{{$i}}</option>
+                                        @endfor
+                                    </select>
+                                </span>
+                                <input type="hidden" name="id" value="{{ $product->id }}">
+                                <div class="form-bottom">カートに入れる</div>
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endif
+        @endforeach
+        <h2>DRINK</h2>
+        @foreach ($products as $product)
+            @if ($product->genre_id == 3)
+                <div class="product">
+                    <div class="inner">
+                        <div class="image"><img src="{{ $product->product_image }}" alt="" /></div>
+                        <div class="title"><h3>{{ $product->product_name }}</h3></div>
+                         <div class="praice"><p>{{ $product->productPrice->product_price }}円</p></div>
+                        <div class="text"><p>{{ $product->product_text }}</p></div>
+                        <div class="btn">
+                            <form class="" action="/cart/store" method="post">
+                                <span>
+                                    <select class="" name="sum">
+                                        @for ($i=1; $i <= 10 ; $i++)
+                                        <option value="{{$i}}">{{$i}}</option>
+                                        @endfor
+                                    </select>
+                                </span>
+                                <input type="hidden" name="id" value="{{ $product->id }}">
+                                <div class="form-bottom">カートに入れる</div>
+                                {{ csrf_field() }}
+                            </form>
+                        </div>
+                    </div>
+                </div>
+            @endif
         @endforeach
         </div>
     </div>
