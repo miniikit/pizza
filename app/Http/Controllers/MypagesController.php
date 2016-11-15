3e7350d54e45eb407e305f8e22913ceca146edf8
minion <minion@minion-2.local>
2016-11-15 17:20:34 +0900

    Mypagesの途中

--- app/Http/Controllers/MypagesController.php
+++ app/Http/Controllers/MypagesController.php
@@ -12,14 +12,27 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Collection;
 
 use App\Http\Requests;
+use App\Service\MypageService;
+
+use Illuminate\Support\Facades\DB;  //サービスに移植後削除
 
 class MypagesController extends Controller
 {
     //注文履歴ページ
     public function orderHistory(){
-        return view('mypage.order.history');
+        $contentsArray = DB::table('orders_master')->join('coupons_master','coupons_master.coupon_number','=','orders_master.coupon_id')->where('orders_master.state_id',1)->get();
+
+           // $object = new MypageService();
+           // $index = $object->showHistory();
+           // list($subject,$b) = $index;
+           // dd($subject,$b);
+
+        list($contents) = $contentsArray;
+        dd($contents);
+        return view('mypage.order.history',compact('$index'));
     }
 
     //注文詳細ページ
--- resources/views/mypage/order/history.blade.php
+++ resources/views/mypage/order/history.blade.php
@@ -1,9 +1,10 @@
 @extends('template/master')
 
-@section('title', 'トップ')
+@section('title', '注文履歴')
 
 @section('css')
     <link rel="stylesheet" href="/css/index/index.css" media="all" title="no title">
+    <link rel="stylesheet" href="/css/mypage/index.css" media="all" title="no title">
 @endsection
 
 @section('plug')
@@ -11,14 +12,37 @@
     <link rel="stylesheet" href="/plug/flexSlier/flexslider.css" media="screen" title="no title">
 @endsection
 @section('main')
+
+
     <div class="container wrap">
-        <div class="order-history">
-            <table class="history">
-                <th>注文日</th>
-                <th>合計</th>
-                <td>2016/8/10</td>
-                <td>¥ 5,000</td>
-            </table>
+        <div class="fonts"><h2>注文履歴</h2></div>
+        <div id="order">
+            <a href="#">
+            <ul>
+                <!--    繰り返し  !-->
+                <li class="history">
+                    <div class="head">
+                        <div class="date mini">注文日</div>
+                        <div class="pay mini">注文金額</div>
+                    </div>
+                    <div class="contents">
+                        <div class="date mini">2016/8/10</div>
+                        <div class="pay mini">¥ 5,000</div>
+                    </div>
+                </li>
+                <!--    繰り返し  !-->
+                <li class="history">
+                    <div class="head">
+                        <div class="date mini">注文日</div>
+                        <div class="pay mini">注文金額</div>
+                    </div>
+                    <div class="contents">
+                        <div class="date mini">2016/10/10</div>
+                        <div class="pay mini">¥ 3,000</div>
+                    </div>
+                </li>
+            </ul>
+            </a>
         </div>
     </div>
 @endsection
