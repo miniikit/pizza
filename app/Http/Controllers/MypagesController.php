b3f5d042551b1e49d0a2abd38a6372325bfafd18
minion <minion@minion-2.local>
2016-11-20 15:03:42 +0900

    Mypages/History完成

--- app/Http/Controllers/MypagesController.php
+++ app/Http/Controllers/MypagesController.php
@@ -26,77 +26,51 @@ use Illuminate\Support\Facades\DB;  //サービスに移植後削除
 class MypagesController extends Controller
 {
     //注文履歴ページ
-    public function orderHistory(){
-
-        //最後に、このif文に処理を入れる
-        if(Auth::check()){  //ログインしている
-            $userId = Auth::user()->id;
-        }else{  //ログインしていない
-            $userId = 4;    //ちかざわさんをテストでいれる
+    //課題：１ページ１０件とかになれば、処理が変わる。
+    //メモ：selectを、valueにすると、ずつ取得するから、for文で回すのに有効か？
+    //開発の余地：order_statesが1の場合、制作済み。2の場合、まだ。って処理を追加？
+       public function orderHistory()
+    {
+        //認証済み？
+        if (Auth::check()) {
+            //ユーザIDを取得
+                $userId = Auth::user()->id;
+            //注文IDごとに、GROUPBY して、「注文日」「注文番号」「合計金額」「クーポン（NULL/int）」を取得。
+                $orders = DB::table('orders_master')->leftjoin('coupons_master', 'coupons_master.coupon_number', '=', 'orders_master.coupon_id')->join('users', 'users.id', '=', 'orders_master.user_id')->join('orders_details_table', 'orders_details_table.id', '=', 'orders_master.id')->join('products_prices_master', 'products_prices_master.id', '=', 'orders_details_table.price_id')->where('users.id', $userId)->select('orders_details_table.id',DB::raw('SUM(products_prices_master.product_price * orders_details_table.number) as total_price'),'orders_master.order_date','coupons_master.coupon_discount')->groupby('orders_details_table.id','orders_master.order_date','coupons_master.coupon_discount')->get();
+            //取得した項目を、送信。
+                return view('mypage.order.history', [ "orders" => $orders]);
+        } else {
             //アクセスエラーページに飛ばす。 Permission Denied.
+            return ('ログインしてください！');
         }
 
-        //課題：１ページ１０件とかになれば、処理が変わる。
-        //メモ：selectを、valueにすると、ずつ取得するから、for文で回すのに有効か？
 
-        //このへんで、　注文IDが何個あるかをDBからカウントし、その回数分forを回してとる方法に変更か。
-            //ユーザIDがログイン中と一致する人の、注文総件数は何件か。
-            $orderCount = 0;
-            $orderCount = DB::table('orders_master')->join('users','orders_master.user_id','=','users.id')->where('users.id','=',$userId)->count();
-
-            //注文回数文、繰り返す。
-            for($i=0; $i<$orderCount; $i++){
-                //繰り返し処理が入る。
-
-            }
-
-        //ユーザID一致の、注文一覧を取得する。注文IDが異なってても、その人の注文をすべて。
-        $contentsArray = DB::table('orders_master')->leftjoin('coupons_master','coupons_master.coupon_number','=','orders_master.coupon_id')->join('users','users.id','=','orders_master.user_id')->join('orders_details_table','orders_details_table.id','=','orders_master.id')->join('products_prices_master','products_prices_master.id','=','orders_details_table.price_id')->where('users.id',$userId)->select('orders_master.id','orders_master.order_date','orders_master.order_appointment_date','orders_master.state_id','coupons_master.coupon_discount','products_prices_master.product_price','orders_details_table.number')->get();
-
-        $contentsArray2 = DB::table('orders_master')->leftjoin('coupons_master','coupons_master.coupon_number','=','orders_master.coupon_id')->join('users','users.id','=','orders_master.user_id')->join('orders_details_table','orders_details_table.id','=','orders_master.id')->join('products_prices_master','products_prices_master.id','=','orders_details_table.price_id')->where('users.id',$userId)->groupby('orders_master.id');
-        dd($contentsArray2);
-        //商品金額×個数を求める。
-        $sum = 0;
-        foreach($contentsArray as $item){
-            $price = $item->product_price;
-            $num = $item->number;
-            $sum += ($price * $num);
-        }
-
-        //クーポンを値引きする(0=>coupon_discount:500とか。foreachかlist使う必要あり。または、注文IDを固定するか。)
-        $orderCoupon = DB::table('orders_master')->join('users','orders_master.user_id','=','users.id')->where('users.id','=',$userId)->join('coupons_master','coupons_master.coupon_number','=','orders_master.coupon_id')->select('coupons_master.coupon_discount')->get();
-        dd($orderCoupon);
-        list($contents) = $contentsArray;
-        dd($contents);
-
-        // ->where('orders_master.state_id',1)これはいらない
-
-           // $object = new MypageService();
-           // $index = $object->showHistory();
-           // list($subject,$b) = $index;
-           // dd($subject,$b);
-
-
-        return view('mypage.order.history',compact('$index'));
     }
 
     //注文詳細ページ
-    public function orderDetail(){
+    //ユーザID一致の、注文一覧を取得する。注文IDが異なってても、その人の注文をすべて。
+    // 重要　$contentsArray = DB::table('orders_master')->leftjoin('coupons_master', 'coupons_master.coupon_number', '=', 'orders_master.coupon_id')->join('users', 'users.id', '=', 'orders_master.user_id')->join('orders_details_table', 'orders_details_table.id', '=', 'orders_master.id')->join('products_prices_master', 'products_prices_master.id', '=', 'orders_details_table.price_id')->where('users.id', $userId)->select('orders_master.id', 'orders_master.order_date', 'orders_master.order_appointment_date', 'orders_master.state_id', 'coupons_master.coupon_discount', 'products_prices_master.product_price', 'orders_details_table.number')->get();
+
+    public function orderDetail()
+    {
         return view('mypage.order.detail');
     }
 
     //登録情報確認ページ
-    public function detail(){
+    public function detail()
+    {
         return view('mypage.detail');
     }
 
     //登録情報編集ページ
-    public function edit(){
+    public function edit()
+    {
         return view('mypage.edit');
     }
 
     //更新確認ページ
-    public function confirm(){
+    public function confirm()
+    {
         return view('mypage.confirm');
     }
 }
--- resources/views/mypage/order/history.blade.php
+++ resources/views/mypage/order/history.blade.php
@@ -11,38 +11,34 @@
     <script src="/plug/flexSlier/jquery.flexslider-min.js" charset="utf-8"></script>
     <link rel="stylesheet" href="/plug/flexSlier/flexslider.css" media="screen" title="no title">
 @endsection
+
 @section('main')
-
-
     <div class="container wrap">
         <div class="fonts"><h2>注文履歴</h2></div>
         <div id="order">
-            <a href="#">
             <ul>
-                <!--    繰り返し  !-->
-                <li class="history">
-                    <div class="head">
-                        <div class="date mini">注文日</div>
-                        <div class="pay mini">注文金額</div>
-                    </div>
-                    <div class="contents">
-                        <div class="date mini">2016/8/10</div>
-                        <div class="pay mini">¥ 5,000</div>
-                    </div>
-                </li>
-                <!--    繰り返し  !-->
-                <li class="history">
-                    <div class="head">
-                        <div class="date mini">注文日</div>
-                        <div class="pay mini">注文金額</div>
-                    </div>
-                    <div class="contents">
-                        <div class="date mini">2016/10/10</div>
-                        <div class="pay mini">¥ 3,000</div>
-                    </div>
-                </li>
+                @foreach($orders as $details)
+                        {{--  クーポン使用後の金額を求め、日付のフォーマットをY/m/dに。  --}}
+                <?php
+                        $totalPrice = $details->total_price;
+                        $coupon = $details->coupon_discount;
+                        $price = $totalPrice-$coupon;
+                        $date = Carbon\Carbon::parse($details->order_date)->format('Y年m月d日');
+                ?>
+                <a href="/order/detail/{{  $details->id }}">
+                    <li class="history">
+                        <div class="head">
+                            <div class="date mini">注文日</div>
+                            <div class="pay mini">注文金額</div>
+                        </div>
+                        <div class="contents">
+                            <div class="date mini"><?php echo $date ?></div>
+                            <div class="pay mini">¥ {{  number_format($price) }}</div>
+                        </div>
+                    </li>
+                </a>
+                @endforeach
             </ul>
-            </a>
         </div>
     </div>
 @endsection
