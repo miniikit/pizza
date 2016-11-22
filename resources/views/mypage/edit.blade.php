2be4b84c27635c399667919028231b470a1816c2
miniikit <b5122@oic.jp>
2016-11-22 14:43:04 +0900

    登録情報編集ページのデザインをbetaに変更

--- resources/views/mypage/edit.blade.php
+++ resources/views/mypage/edit.blade.php
@@ -21,24 +21,23 @@
                     <?php
                     $zip = $user->postal;
                     $postal = substr($zip, 0, 3).'-'.substr($zip, 3);
-                    $gender = "男";
                     ?>
-                    @if($user->gender_id == 2)
-                        $gender = 女
-                    @endif
-                    <tr><th>名前</th><td><input type="text" size="20" maxlength="50" value="{{ $user->name }}"></td></tr>
-                    <tr><th>フリガナ</th><td><input type="text" size="20" maxlength="50" value="{{ $user->kana }}"></td></tr>
-                    <tr><th>郵便番号</th><td>〒<input type="text" size="20" maxlength="7" value="{{ $postal }}"></td></tr>
-                    <tr><th>住所１</th><td><input type="text" size="40" maxlength="50" value="{{ $user->address1 }}"></td></tr>
-                    <tr><th>住所２</th><td><input type="text" size="20" maxlength="30" value="{{ $user->address2 }} {{ $user->address3 }}"></td></tr>
-                    <tr><th>生年月日</th><td><input type="date" size="20" maxlength="50" value="{{ date('Y年 n月 j日', strtotime($user->birthday)) }}"></td></tr>
-                    <tr><th>電話番号</th><td><input type="tel" size="20" maxlength="11" value="{{ $user->phone }}"></td></tr>
-                    <tr><th>性別</th><td><input type="radio" size="20" maxlength="50" value="{{ $gender }}"></td></tr>
-                    <tr><th>メールアドレス</th><td><input type="email" size="20" maxlength="50" value="{{ $user->email }}"></td></tr>
-                    <tr><th>パスワード</th><td>**********</td></tr>
-                    <tr><th>新しいパスワード</th><td><input type="password" size="20"  value=""></td></tr>
-                    <tr><th>新しいパスワード</th><td><input type="password" size="20"  value=""></td></tr>
+                    <tr><th>名前</th><td><input type="text" size="20" maxlength="50" value="{{ $user->name }}"></td><td>（例）大阪太郎</td></tr>
+                    <tr><th>フリガナ</th><td><input type="text" size="20" maxlength="50" value="{{ $user->kana }}"></td><td>（例）オオサカタロウ</td></tr>
+                    <tr><th>郵便番号</th><td>〒 <input type="text" placeholder="123456" size="7" maxlength="7" value="{{ $user->postal }}"></td><td>（例）123456</td></tr>
+                    <tr><th>住所１</th><td><input type="text" placeholder="大阪府大阪市天王寺区" size="40" maxlength="50" value="{{ $user->address1 }}"></td><td>（例）大阪府大阪市天王寺区</td></tr>
+                    <tr><th>住所２</th><td><input type="text" placeholder="１−１−１" size="40" maxlength="50" value="{{ $user->address2 }}"></td><td>番地</td></tr>
+                    <tr><th>住所３</th><td><input type="text" placeholder="マンション５０２" size="40" maxlength="50" value="{{ $user->address3 }}"></td><td>建物名</td></tr>
+                    <tr><th>生年月日</th><td><input type="date" size="20" maxlength="50" value="{{ date('Y-0n-0j', strtotime($user->birthday)) }}"></td><td></td></tr>
+                    <tr><th>電話番号</th><td><input type="tel" placeholder="08012345678" size="20" maxlength="11" value="{{ $user->phone }}"></td><td>ハイフンなし</td></tr>
+                    <tr><th>性別</th><td><input type="radio" name="gender" value="男" <?if($user->gender_id === 1){echo "checked";}?>>男 <input type="radio" name="gender" value="女" <?if($user->gender_id === 2){echo "checked";}?>>女</td><td></td></tr>
+                    <tr><th>メールアドレス</th><td><input type="email" size="20" maxlength="50" value="{{ $user->email }}"></td><td>aaa@yahoo.co.jp</td></tr>
+                    <tr><th>新しいパスワード</th><td><input type="password" placeholder="任意" size="20" value=""></td><td>パスワード変更</td></tr>
+                    <tr><th>新しいパスワード</th><td><input type="password" placeholder="確認" size="20" value=""></td><td>パスワード変更確認用</td></tr>
+                    <div class = "space"></div>
+                    <tr><th>変更用パスワード</th><td><input type="password" placeholder="必ず入力" size="20" value=""></td><td>必須</td></tr>
                 @endforeach
+
             </table>
             <form class="submit" action="/mypage/edit" method="post">
                 <a class="form-bottom" href="#">編集</a>
