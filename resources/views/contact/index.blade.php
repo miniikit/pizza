c8c5f74b5559aed0cde9e0433b82e0ac549a5b75
in-the-box <hacoco345@gmail.com>
2016-11-22 21:10:00 +0900

    contact バリデーション追加

--- resources/views/contact/index.blade.php
+++ resources/views/contact/index.blade.php
@@ -14,13 +14,22 @@
 @section('main')
     <div class="container wrap">
         <h2>お問い合わせ</h2>
+        @if (count($errors) > 0)
+            <div class="error">
+                <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                </ul>
+            </div>
+        @endif
          <div id="contact">
           <form action="/contact" method="post">
               <ul id="from-group">
                 <li>メールアドレス</li>
-                <li><input type="text" name="email" label="email" class="form-mail"></li>
+                <li><input type="text" name="email" class="form-mail"></li>
                 <li>内容</li>
-                <li><textarea name="textarea" label="text" rows="10" cols="100"></textarea></li>
+                <li><textarea name="body" rows="10" cols="100"></textarea></li>
               </ul>
               <div class="form-bottom"><a>送信</a></div>
               {{ csrf_field() }}
