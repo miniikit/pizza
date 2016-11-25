ad3999b6bf2b871ea552e1fdb079beb3e87f3642
in-the-box <hacoco345@gmail.com>
2016-11-25 15:14:11 +0900

    電話注文デザイン編集

--- resources/views/pizzzzza/order/accept/input.blade.php
+++ resources/views/pizzzzza/order/accept/input.blade.php
@@ -7,27 +7,15 @@
 @endsection
 
 @section('main')
-    <div class="wrap">
-      <h1>電話番号受付</h1>
-      <div id="tel">
-        <h2 class="text-center">顧客情報確認</h2>
-        <h3 class="text-center">電話番号入力</h3>
-        <form class="form-inline">
-
-          <div class="form-group">
-              <input class="form-control"  type="text" name="name" value="">-
-          </div>
-
-          <div class="form-group">
-              <input type="text" name="password" class="form-control">-
-          </div>
-
-          <div class="form-group">
-              <input type="text" name="password" class="form-control">
-          </div>
-            <button type style="margin-top:20px;" ="button" class="btn btn-primary btn-lg"name="button">確認</button>
-          </form>
-
-      </div>
-    </div>
+<div class="wrap">
+<h1>電話注文</h1>
+<div id="tel">
+<form class="" action="index.html" method="post">
+<div class="form-group">
+<input type="text" class="form-control" placeholder="電話番号を入力してください" name="tel" value="" >
+</div>
+<button type="button" class="btn btn-primary btn-lg btn-block">確認</button>
+</form>
+</div>
+</div>
 @endsection
