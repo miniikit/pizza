d52f85b449a1dc8f46b7a5333218d734e4d532e7
miniikit <b5122@oic.jp>
2016-11-26 01:50:46 +0900

    a

--- app/Http/Controllers/AdminMenusController.php
+++ app/Http/Controllers/AdminMenusController.php
@@ -14,19 +14,165 @@ use Carbon\Carbon;
 use App\Http\Requests;
 use App\Product;
 
+use Illuminate\Support\Facades\DB;
+use phpDocumentor\Reflection\Types\Integer;  //サービスに移植後削除
+
 class AdminMenusController extends Controller
 {
 
     public function index()
     {
-        $products = Product::with('productPrice')->get();
-        return view('pizzzzza.menu.index', compact('products'));
+        //
+        //　商品の情報と価格を連結
+        //`
+
+
+        $products = DB::table('products_master')->join('products_prices_master','products_master.price_id','=','products_prices_master.id')->get();
+
+
+        /*
+          +"id": 1
+          +"product_name": "明太バターチーズ"
+          +"price_id": 1
+          +"product_image": "/images/product/1.jpg"
+          +"product_text": "大きくカットしたポテトにコーンとベーコンをトッピングして、明太クリームソース、バター、チーズを合わせた、家族で楽しめるピザです。"
+          +"genre_id": 1
+          +"sales_start_date": "2016-10-10"
+          +"sales_end_date": null
+          +"created_at": "2016-11-24 17:47:35"
+          +"updated_at": "2016-11-24 17:47:35"
+          +"deleted_at": null
+          +"product_id": 1
+          +"product_price": 1990
+          +"price_change_startdate": "2016-10-10"
+          +"price_change_enddate": null
+          +"employee_id": 1
+         */
+
+        return view('pizzzzza.menu.index', compact('products') );
     }
 
-    public function edit()
-    {
-        return view('pizzzzza.menu.edit');
+
+
+
+
+    //
+    public function push(){
+
+        return redirect('pizzzzza.menu');
     }
+
+
+
+
+    // メニュー画面:: 編集 or 販売終了ボタンが押される。
+    public function edit(Request $request)
+    {
+        //  処理内容
+        //      「選択商品を販売終了（ソフトデリート）」または「選択商品の、編集ページへリダイレクト」する。
+        //      渡された値によって処理を振り分ける。
+        //  期待値
+        //      $request->menu  >>  editかendが入っている。これによって処理を振り分け。
+        //      $request->id  >>   選択された商品（１つ）に対し、処理を行う。
+
+
+        // $message・・・「販売終了」が押された場合の、エラーメッセージ/処理状況/クラスの返却。
+        //     $message["status"] : 状態(error/end_ok)
+        //     $message["text"] : 表示内容(画面に表示)
+        //     $message["class"] : 付加CSSのClass(色でユーザに示す）
+        $message = array();
+
+
+        //
+        //  エラー処理
+        //
+
+            //ボタンを経由せずに飛んできた場合（JSの不正実行には対応できない）
+            if(!$request->has('menu')){
+                //ボタンが押されずにとんできた、不正な処理
+                return redirect('/pizzzzza/login');
+            }
+
+            //商品が選択されていない場合の処理
+            if(!$request->has('id')){
+                //エラーメッセージを返す予定
+                $message["status"] = "error";
+                $message["text"] = "エラー：商品を選択してください";
+                $message["class"] = "menu menu-error";
+                return redirect('/pizzzzza/menu')->with('message',$message);
+            }
+
+
+        //
+        //  フォームからの値を取得。
+        //
+
+            //  どちらのボタンが押されたか判定
+            $judge = htmlspecialchars($request->menu); //"edit" or "end"が入る
+
+            //  選択された商品のIDを取得する。
+            $productId = htmlspecialchars($request->id);
+
+
+        //
+        //  処理１：編集ボタンが押された場合。（編集ページへのリダイレクト）
+        //
+
+            if($judge == "edit"){
+
+                // DBから該当商品の情報を取得
+                $products = DB::table('products_master')->where('id','=',$productId)->first();
+
+                dd($products);
+                return redirect('pizzzzza/menu/edit')->with('products',$products);
+            }
+
+
+        //
+        //  処理２：販売終了ボタンが押された場合。（この中で販売を実際に終了させる。販売終了日に現在日時を設定する。）
+        //
+
+            //
+            //  処理２−１：既にソフトデリートされているものか確認する（本当はAjaxの方が良いが）
+            //
+
+            if($judge == "end"){
+                $value = DB::table('products_master')->where('id', $productId)->first();
+                if(!is_null($value->deleted_at)){
+                    $message["status"] = "error";
+                    $message["text"] = "既に販売が終了された商品です。";
+                    $message["class"] = "menu menu-error";
+
+                    return redirect('/pizzzzza/menu')->with('message',$message);
+                }
+            }
+
+            //
+            //  処理２−２：実際にソフトデリートする
+            //
+            if($judge == "end"){
+
+                $now = Carbon::now();
+
+                //手動でのソフトデリート。
+                DB::table('products_master')->where('id', $productId)->update(['deleted_at' => $now]);
+                $message["status"] = "end_ok";
+                $message["text"] = "選択された商品の販売を終了しました。";
+                $message["class"] = "menu menu-end-complete";
+
+                return redirect('/pizzzzza/menu')->with('message',$message);
+            }
+
+
+        //
+        //  最終エラー処理（たどりつくはずのない処理）
+        //
+        return redirect('pizzzzza/login');
+    }
+
+
+
+
 
 
     public function add()
--- resources/views/pizzzzza/menu/index.blade.php
+++ resources/views/pizzzzza/menu/index.blade.php
@@ -6,62 +6,103 @@
     <link rel="stylesheet" href="/css/pizzzzza/menu/index.css" media="all" title="no title">
 @endsection
 
+@section('js')
+    <script>
+        function postEdit() {
+            var element = document.getElementById("postButton");
+            element.setAttribute("value", "edit");
+            event.preventDefault();
+            document.getElementById('edit').submit();
+        }
+        function postEnd() {
+            var element = document.getElementById("postButton");
+            element.setAttribute("value", "end");
+            event.preventDefault();
+            document.getElementById('edit').submit();
+        }
+    </script>
+@endsection
+
 @section('main')
-      <h1>メニュー管理画面</h1>
-      <div id="menu_button">
-        <button type="button" class="btn btn-primary btn-lg"name="button">編集</button>
-        <a class="btn btn-danger btn-lg" data-featherlight="#delete">削除</a>
-        {{-- <button type="button" class="btn btn-primary btn-lg"name="button">追加</button> --}}
-      </div>
+    <h1>メニュー管理画面</h1>
 
-      <div id="delete" class="delete">
-          <div class="inner">
-              本当に削除しますか？
-              <div class="button">
-                  <a class="btn btn-default" aria-label="Close" onclick="$.featherlight.current().close();">キャンセル</a>
-                  <a class="btn btn-danger">　削除　</a>
-              </div>
-          </div>
-      </div>
+    <div class="form-group table-responsive">
 
-      <div class="form-group table-responsive">
-        <table class="table">
-          <thead>
-            <tr>
-              <th></th>
-              <th style="text-align: center;">ID</th>
-              <th>メニュー名</th>
-              <th>画像データ</th>
-              <th>説明</th>
-              <th style="text-align: center;">価格</th>
-              <th style="text-align: center;">販売開始日</th>
-              <th style="text-align: center;">販売終了日</th>
-            </tr>
-          </thead>
-          <tbody>
-              <form class="" action="index.html" method="post">
-                  @foreach ($products as $product)
-                  <tr>
-                    <td style="text-align: center;"><input type="checkbox" name="id[]" value="{{ $product->id }}"></td>
-                    <td style="width:5%;text-align: center;">{{ $product->id }}</td>
-                    <td style="width:20%;">{{ $product->product_name }}</td>
-                    <td style="width:20%;"><img src="{{ $product->product_image }}" alt="{{ $product->product_name }}" /></td>
-                    <td style="width:25%;">{{ $product->product_text }}</td>
-                    <td style="text-align: center;">{{ number_format($product->productPrice->product_price)}}円</td>
-                    <td style="text-align: center;">{{ $product->sales_start_date }}</td>
-                    <td style="text-align: center;">
-                    @if ($product->sales_end_date == null)
-                        未設定
-                    @else
-                        {{ $product->sales_end_date }}
-                    @endif
-                    </td>
-                  </tr>
-                  @endforeach
-                  {{-- <form method="post" enctype="multipart/form-data"><input type="file" name="pic"></form> --}}
-              </form>
-          </tbody>
-        </table>
-        </div>
+        <form id="edit" action="/pizzzzza/menu/edit" method="get">
+            <lavel name="select_button">
+                <div id="menu_button">
+                    <a type="button" class="btn btn-primary btn-lg" href="#"
+                       onclick="postEdit()">編集</a>
+                    <input id="postButton" type="hidden" name="menu" value="" style="display: none">
+                    <a class="btn btn-danger btn-lg" data-featherlight="#delete" href="#">販売終了</a>
+                    {{-- <button type="button" class="btn btn-primary btn-lg"name="button">追加</button> --}}
+                </div>
+
+                <div id="delete" class="delete">
+                    <div class="inner">
+                        選択した商品の販売を本当に終了しますか？
+                        <div class="button">
+                            <a class="btn btn-default" aria-label="Close" onclick="$.featherlight.current().close();">キャンセル</a>
+                            <a class="btn btn-danger" href="#" onclick="postEnd()">　販売を終了　</a>
+                        </div>
+                    </div>
+                </div>
+            </lavel>
+
+            @if(session()->has('message'))
+                <?php $a = session()->pull('message'); ?>
+                <div class="{{$a["class"]}}">
+                    {{ $a["text"] }}
+                </div>
+            @endif
+
+            <table class="table">
+                <thead>
+                <tr>
+                    <th></th>
+                    <th style="text-align: center;">ID</th>
+                    <th>メニュー名</th>
+                    <th>画像データ</th>
+                    <th>説明</th>
+                    <th style="text-align: center;">価格</th>
+                    <th style="text-align: center;">販売開始日</th>
+                    <th style="text-align: center;">販売終了日</th>
+                </tr>
+                </thead>
+                <tbody>
+                @if($products)
+                    @foreach ($products as $product)
+                        @if(is_null($product->deleted_at))
+                            <tr>
+                        @else
+                            <tr class="menu-deleted">
+                        @endif
+                            <td style="text-align: center;"><input type="radio" name="id" value="{{ $product->id }}">
+                            </td>
+                            <td style="width:5%;text-align: center;">{{ $product->id }}</td>
+                         @if(is_null($product->deleted_at))
+                            <td style="width:20%;">{{ $product->product_name }}</td>
+                         @else
+                             <td style="width:20%;">{{ $product->product_name }}<br><b class="red">※販売終了</b></td>
+                         @endif
+                            <td style="width:20%;"><img src="{{ $product->product_image }}"
+                                                        alt="{{ $product->product_name }}"/></td>
+                            <td style="width:25%;">{{ $product->product_text }}</td>
+                            <td style="text-align: center;">{{ number_format($product->product_price )}}円</td>
+                            <td style="text-align: center;">{{ $product->sales_start_date }}</td>
+                            <td style="text-align: center;">
+                                @if ($product->sales_end_date == null)
+                                    未設定
+                                @else
+                                    {{ $product->sales_end_date }}
+                                @endif
+                            </td>
+                        </tr>
+                    @endforeach
+                @endif
+                </tbody>
+            </table>
+        </form>
+    </div>
     </div>
 @endsection
