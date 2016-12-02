0fffdf3aa76b3d2443a0bfabb5c2bcfade106369
miniikit <b5122@oic.jp>
2016-12-02 16:26:31 +0900

    クーポン途中

--- resources/views/pizzzzza/coupon/list/edit.blade.php
+++ resources/views/pizzzzza/coupon/list/edit.blade.php
@@ -7,90 +7,104 @@
 @endsection
 
 @section('pankuzu')
-  <ol class="breadcrumb">
-    <li><a href="/pizzzzza/order/top">ホーム</a></li>
-    <li><a href="/pizzzzza/coupon/menu">クーポンメニュー</a></li>
-    <li><a href="/pizzzzza/coupon/list">開催中クーポン一覧</a></li>
-    <li class="active">クーポン編集</li>
-  </ol>
+    <ol class="breadcrumb">
+        <li><a href="/pizzzzza/order/top">ホーム</a></li>
+        <li><a href="/pizzzzza/coupon/menu">クーポンメニュー</a></li>
+        <li><a href="/pizzzzza/coupon/list">開催中クーポン一覧</a></li>
+        <li class="active">クーポン編集</li>
+    </ol>
 @endsection
 
 @section('main')
-  <h1>クーポン編集</h1>
-  <div class="row">
-    <div class="col-md-7">
-      <table class="table table-bordered">
-        <tbody>
-        <tr>
-          <th class="text-center">クーポン名</th>
-          <td><input type="text" name="" value="{{ $coupon->coupon_name }}"></td>
-        </tr>
-        <tr>
-          <th class="text-center">クーポン番号</th>
-          <td><input type="text" name="" value="{{ $coupon->coupon_number }}"></td>
-        </tr>
-        <tr>
-          @if($coupon->coupon_discount < 0)
-            <th class="text-center">プレゼント商品名</th>
-            <td><input type="text" name="" value=""></td>
-          @else
-            <th class="text-center">値引額</th>
-            <td><input type="text" name="" value="{{ $coupon->coupon_discount }}"></td>
-          @endif
-        </tr>
-        <tr>
-          <th class="text-center">利用上限回数</th>
-          <td><input type="text" name="" value="{{ $coupon->coupon_conditions_count }}"></td>
-        </tr>
-        <tr>
-          <th class="text-center">利用条件金額</th>
-          <td><input type="text" name="" value="{{ $coupon->coupon_conditions_money }}"></td>
-        </tr>
-        <tr>
-          <th class="text-center">対象者</th>
-          <td><input type="text" name="" value="{{ $coupon->coupon_conditions_first }}"></td>
-        </tr>
-        <tr>
-          <th class="text-center">クーポン種別</th>
-          <td>
-            <select name="coupon_type_id" id="">
-              <option value="{{ $coupon->coupon_id }}">{{ $coupon->couponType }}</option>
-            </select>
-          </td>
-        </tr>
-        </tbody>
-      </table>
-    </div>
-    <div class="col-md-5">
-      <table class="table table-bordered">
-        <tbody>
-        <tr>
-          <th class="text-center">開始日</th>
-          <td>{{ $coupon->coupon_start_date }}</td>
-        </tr>
-        <tr>
-          <th class="text-center">終了日</th>
-          <td><input type="date" name="" value="{{ $coupon->coupon_end_date }}"></td>
-        </tr>
-        <tr>
-          <th class="text-center">登録日時</th>
-          <td>{{ $coupon->created_at }}</td>
-        </tr>
-        <tr>
-          <th class="text-center">更新日時</th>
-          <td>{{ $coupon->updated_at }}</td>
-        </tr>
-        </tbody>
-      </table>
+    <h1>クーポン編集</h1>
+    <div class="row">
+        <div class="col-md-7">
+            <form action="/pizzzzza/coupon/{{$id}}/update" method="post">
+                <table class="table table-bordered">
+                    <tbody>
+                    <tr>
+                        <th class="text-center">クーポン名</th>
+                        <td><input class="form-control" type="text" name="coupon_name" value="{{ $coupon->coupon_name }}"></td>
+                    </tr>
+                    <tr>
+                        <th class="text-center">クーポン番号</th>
+                        <td><input class="form-control" type="text" name="coupon_num" value="{{ $coupon->coupon_number }}"></td>
+                    </tr>
+                    <tr>
+                            <th class="text-center">値引額</th>
+                            <td><input class="form-control" type="text" name="coupon_discount" value="{{ $coupon->coupon_discount }}">
+                            </td>
+                    </tr>
+                    <tr>
+                        <th class="text-center">利用上限回数</th>
+                        <td><input class="form-control" type="text" name="coupon_max"
+                                   value="{{ $coupon->coupon_conditions_count }}"></td>
+                    </tr>
+                    <tr>
+                        <th class="text-center">利用条件金額</th>
+                        <td><input class="form-control" type="text" name="coupon_conditions_price"
+                                   value="{{ $coupon->coupon_conditions_money }}"></td>
+                    </tr>
+                    <tr>
+                        <th class="text-center">対象者</th>
+                        <td><select class="form-control" name="coupon_conditions_first" id="">
+                                @if($coupon->coupon_conditions_first == 1)
+                                    <option value="0">全員</option>
+                                    <option value="1" checked>当店初回利用者限定</option>
+                                @else
+                                    <option value="0" checked>全員</option>
+                                    <option value="1">当店初回利用者限定</option>
+                                @endif
+                            </select></td>
+                    </tr>
+                    <tr>
+                        <th class="text-center">クーポン種別</th>
+                        <td>
+                            <select class="form-control" name="coupon_type_id" id="">
+                                @foreach($couponTypes as $type)
+                                    @if($type->id == $coupon->coupons_types_id)
+                                        <option value="{{ $type->id }}" checked>{{ $type->coupon_type }}</option>
+                                    @else
+                                        <option value="{{ $type->id }}">{{ $type->coupon_type }}</option>
+                                    @endif
+                                @endforeach
+                            </select>
+                        </td>
+                    </tr>
+                    </tbody>
+                </table>
+        </div>
+        <div class="col-md-5">
+            <table class="table table-bordered">
+                <tbody>
+                <tr>
+                    <th class="text-center">開始日</th>
+                    <td>{{ $coupon->coupon_start_date }}</td>
+                </tr>
+                <tr>
+                    <th class="text-center">終了日</th>
+                    <td><input class="form-control" type="date" name="coupon_end_date" value="{{ $coupon->coupon_end_date }}"></td>
+                </tr>
+                <tr>
+                    <th class="text-center">登録日時</th>
+                    <td>{{ $coupon->created_at }}</td>
+                </tr>
+                <tr>
+                    <th class="text-center">更新日時</th>
+                    <td>{{ $coupon->updated_at }}</td>
+                </tr>
+                </tbody>
+            </table>
+            <div class="ar">
+                <a href="/pizzzzza/coupon/{{$id}}/delete" class="btn btn-danger btn-sm">削除</a>
+                <input class="btn btn-primary btn-sm ml" type="submit" name="status" value="更新">
+            </div>
+            {{ csrf_field() }}
+            </form>
 
-      <form class="ar" action="/pizzzzza/coupon/{{$coupon->id}}/delete" method="post">
-        <a href="/pizzzzza/coupon/{{$coupon->id}}/update" class="btn btn-primary btn-sm">更新</a>
-        {{ csrf_field() }}
-      </form>
-
+        </div>
+        <div class="col-md-4 col-md-offset-4 mt">
+            <a href="/pizzzzza/coupon/{{$coupon->id}}/show" class="btn btn-default btn-lg btn-block">戻る</a>
+        </div>
     </div>
-    <div class="col-md-4 col-md-offset-4 mt">
-      <a href="/pizzzzza/coupon/list" class="btn btn-default btn-lg btn-block">戻る</a>
-    </div>
-  </div>
 @endsection
