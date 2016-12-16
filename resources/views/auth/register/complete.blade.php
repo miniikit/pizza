c73802529b17fc5df12d8e310ba3a8f3c44bffaf
fd3s7 <B5021@oic.jp>
2016-12-16 16:17:55 +0900

    色々修正

--- resources/views/auth/register/complete.blade.php
+++ resources/views/auth/register/complete.blade.php
@@ -1,25 +1,26 @@
-@extends('template.auth')
+@extends('template.master')
 
 @section('title', '登録完了')
 
 @section('css')
-    <link rel="stylesheet" href="/css/pages/index.css" media="all" title="no title">
+    <link rel="stylesheet" href="/css/auth/register/complete.css" media="all" title="no title">
 @endsection
 
 @section('main')
 <div class="container">
-  <div class="col-md-8 col-md-offset-2">
-  <div class="panel panel-default">
-    <div class="panel-body">
-      <h2>会員完了しました!</h2>
-      この度は、OIC PIZZAをご利用いただき、誠にありがとうございます。<br>
-      次回から、この会員情報をご利用ください。<br>
-      <div class="text-center">
-      <a href="/" class="btn btn-primary">TOPへ</a>
-      <a href="/login" class="btn btn-success">ログイン</a>
+    <div class="wrap">
+      <div class="test-box">
+        <h1>会員完了しました!</h1>
+        <p>
+          この度は、OIC PIZZAをご利用いただき、誠にありがとうございます。
+          次回から、この会員情報をご利用ください。
+        </p>
+      </div>
+
+      <div class="btn-box">
+          <div class="box1"><a href="/">TOPへ</a></div>
+          <div class="box2"><a href="/login">ログイン</a></div>
       </div>
     </div>
-  </div>
-  </div>
 </div>
 @endsection
