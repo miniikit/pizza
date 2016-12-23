8ae9a93f0ef60d5b99d67db7a861567ede4f0030
miniikit <b5122@oic.jp>
2016-12-23 14:10:41 +0900

    管理者商品/キャンペーン::デザイン変更

--- resources/views/pizzzzza/campaign/show.blade.php
+++ resources/views/pizzzzza/campaign/show.blade.php
@@ -92,3 +92,13 @@
         </div>
     </div>
 @endsection
+
+
+@section('script')
+    <script style="text/javascript">
+        {{-- thをwidth:50%に --}}
+        $(document).ready(function () {
+            $('th').addClass('th-50');
+        });
+    </script>
+@endsection
